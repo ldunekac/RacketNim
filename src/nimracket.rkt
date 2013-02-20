00ce3683d4b8b19f1f7c1cf2a5e2b144b64dc651
Luke Dunekacke <ldunekacke@gmail.com>
2013-02-20 11:30:51 -0700

    Contains comments

--- src/nimracket.rkt
+++ src/nimracket.rkt
@@ -1,12 +1,20 @@
 #lang racket
+
+; Luke Dunekacke
+; CS400 2nd racket assignment
+; due 2/21/2013
+
+;; THE GAME OF NIM
+
+;; TEST CONDITIONS
 ;; (nim '((x x x x)(x)) '(human human))
 ;; (nim '((x x x x)(x x x x x)(x x x x x x) (x x x x x x x x)) '(smart smart))
 
-(require racket/trace)
-
+;; Starting point of the game
 (define (nim board players)
   (makeMove (makeNumberBoard board) players (list 1 2)))
 
+;; returns a list of the number of sticks in each row 
 (define (makeNumberBoard board)
   (define (rowCount row acc)
     (cond [(empty? row) acc]
@@ -14,17 +22,21 @@
  (cond [(empty? board) '()]
        [else (cons (rowCount (car board) 0) (makeNumberBoard (cdr board)))]))
 
+;; checks to see if the board is null 
+;; cheks to see if the game is won
+;; diviest he move based on the type of player
 (define (makeMove board players 1or2)
-  (display board)
+  (if (void? board)
+      (display "")
   (if (end? board)
       (won (cadr 1or2)) 
       (begin (printBoard board 1or2)
        (cond [(eq? 'human  (car players)) (humanMove board players 1or2)]
              [(eq? 'random (car players)) (randomMove board players 1or2)]
-             [else (smartMove board players 1or2)]))))
+             [else (smartMove board players 1or2)])))))
 
-(trace makeMove)
 
+;; asks for input from the player
 (define (humanMove board players 1or2)
   (define (take row)
     (display "How many do you want to take? ")
@@ -34,6 +46,7 @@
      (take (read)))
   (getRow))
 
+;; chekcs to see if the palyer entered the correct move
 (define (evalHumanMove board players 1or2 row many)
   (if (validMove? board row many) 
       (begin (display "---------------------")
@@ -44,7 +57,8 @@
              (display "---------------------")
               (newline)
              (makeMove board players 1or2))))
-  
+
+;; Picks a random row and an amount from the board
 (define (randomMove board players 1or2)
   (define (valueAt board row)
     (cond [(empty? board) 0]
@@ -58,7 +72,7 @@
   (pickMany board players 1or2 (+ 1 (random (length board)))))
 
 
-;; TODO !!!!
+;; Makes the best possible move
 (define (smartMove board players 1or2)
   (define (replaceBoard board players 1or2 bitValue)
     (cond [(= bitValue 0) (randomMove board players 1or2)]
@@ -68,24 +82,27 @@
   (makeMove (replaceBoard board players 1or2 (apply bitwise-xor board)) (reverse players) (reverse 1or2)))
 
 ;; Update
+;; removes the amount "many" from a row
 (define (updateBoard board row many)
   (cond [(= row 1) (cons (- (car board) many) (cdr board))]
         [else (cons (car board)(updateBoard (cdr board) (- row 1) many))]))
 
 ;; Conditions statments
+;; test if a move is valid
 (define (validMove? board row many)
   (cond [(empty? board) #f]
         [(< row 1) #f]
         [(<= many 0) #f]
         [(eq? row 1) (<= many (car board))]
         [else (validMove? (cdr board) (- row 1) many)]))
-
+;; checks to see if the game is finished
 (define (end? board)
   (cond [(empty? board) #t]
         [(zero? (car board)) (end? (cdr board))]
         [else #f]))
 
 ;; Printing statments
+;; prints the board and the nexts players turn
 (define (printBoard board 1or2)
   (define (printRow num)
     (cond [(= num 0) (newline)]
@@ -105,6 +122,7 @@
                 (printPrefix (cdr board)(+ 1 rowNum)))]))
   (printPrefix board 1))
 
+;; prints the winning player
 (define (won player)
   (begin
   (display "player ") (display player)(display " won!")))
